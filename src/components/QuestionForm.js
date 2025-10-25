8d5684f207054cdb7978814c63545adaa3f89493
Collins Kibet <kollcibe05@gmail.com>
2025-10-25 13:35:09 +0300

    solved crud lab

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,20 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+  const questionsData ={
+  prompt: formData.prompt,
+  answers : [formData.answer1, formData.answer2, formData.answer3, formData.answer4]
+}
+
+      fetch("http://localhost:4000/questions",{
+       method: "POST",
+       headers:{
+        'Content-Type': 'application/json',
+       }, 
+       body: JSON.stringify(questionsData)
+      })
+      .then(r => r.json())
+      .then(newItem => console.log(newItem))
   }
 
   return (
@@ -84,7 +97,7 @@ function QuestionForm(props) {
             <option value="3">{formData.answer4}</option>
           </select>
         </label>
-        <button type="submit">Add Question</button>
+        <button type="submit" >Add Question</button>
       </form>
     </section>
   );
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React,{useState, useEffect} from "react"
+import QuestionItem from "./QuestionItem";
+
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+useEffect(() => {
+  fetch("http://localhost:4000/questions")
+  .then(r => r.json())
+  .then(data => setQuestions(data))
+},[])
+
+function handleBackDelete (id) {
+  const updatedQuestions = questions.filter( question =>  question.id !== id)
+  setQuestions(updatedQuestions)
+}
+
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{
+      questions.map(question => (
+        <QuestionItem
+        key={question.id}
+        question={question}
+        handleBackDelete={handleBackDelete}
+        />
+      )) 
+    }</ul>
     </section>
   );
 }
